2b8d3dc947a57dd859c787f8f22bdf3899e50e77
Robert Jomar Malate <robjmal@gmail.com>
2017-07-21 12:49:38 -0400

    This is the Python file that contains data-struct

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# This dictionary will store the ingredients as keys and the receipies that one
+# could make as values.
+ingredients_dict = {
+    "eggs" : ["cake", "hard-boiled egg", "ultimate breakfast"],
+    "milk" : ["milkshake", "waffles/pancake", "cereal"],
+    "lettuce" : ["salad", "taco", "burger"],
+    "bread" : ["Peanut Butter & Jelly Sandwich", "Sub", "Pizza"],
+    "chicken" : ["Roast Chicken", "Chicken Soup", "BBQ"],
+}
